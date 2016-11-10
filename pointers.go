3c6ee99f75a96a75ca0509bacabd3c3538faf34e
Metin Demir <metmirr@users.noreply.github.com>
2016-11-10 14:03:33 +0200

    Update pointers.go

--- pointers.go
+++ pointers.go
@@ -1,1 +1,15 @@
+// takes two pointer, change their value with each other
+func swap(x *int, y *int) {
+  temp := *x
+  *x = *y
+  *y = temp
+  
+  fmt.Println(*x, *y)
+}
 
+func main() {
+  x := int(1)
+  y := int(2)
+  // addresses are passing to the swap function
+  swap(&x, &y)
+}
