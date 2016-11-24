652d6cf41d5e3d34a8160f01d675e5f602a0dacd
Metin Demir <metmirr@users.noreply.github.com>
2016-11-24 10:51:39 +0300

    Update me_test.go

--- ornekler/testler/me_test.go
+++ ornekler/testler/me_test.go
@@ -2,10 +2,10 @@
 //
 // Go ile yazilan kodlari test etmek icin:
 //   
-//   * test kodlarini icerecek dosyanin ismi `_test.go` ile bitmeli ve `testing` paketi import edilmeli.
-//	 * test fonksiyonlarin ismi `Test` ismi ile baslamali ve bu fonksiyonlar parametre olarak
+//   - test kodlarini icerecek dosyanin ismi `_test.go` ile bitmeli ve `testing` paketi import edilmeli.
+//   - test fonksiyonlarin ismi `Test` ismi ile baslamali ve bu fonksiyonlar parametre olarak
 //     `*testing.Test` tipinde bir parametre almalidir.
-//	 * hata mesajlari icin fonksiyona verilen parametrenin metodu olan `Error()` fonksiyonu kullanilabilir.
+//   - hata mesajlari icin fonksiyona verilen parametrenin metodu olan `Error()` fonksiyonu kullanilabilir.
 //
 // Test'i baslatmak icin: $ go test
 // Birden fazla test dosyasi kullanacaksaniz her dosyadaki paket ismi ayni olmalidir. Diger turlu hata alirsiniz.
